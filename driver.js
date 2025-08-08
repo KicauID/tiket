83f139f69baa484a248640fff5971da405e4fce0
KicauID <Kicauid.official@gmail.com>
2025-08-08 13:44:27 +0700

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,18 +1,26 @@
 window.addEventListener("message", async function(event) {
-    const { origin, data: { key, params } } = event;
-  
+    const { data: { key, params } } = event;
+
     let result, error;
     try {
+        // Hanya meneruskan param HTML
         result = await window.function(...params);
     } catch (e) {
         result = undefined;
-        try { error = e.toString(); }
-        catch { error = "Exception can't be stringified."; }
+        try {
+            error = e.toString();
+        } catch {
+            error = "Exception can't be stringified.";
+        }
     }
-  
+
     const response = { key };
-    if (result !== undefined) response.result = { value: result };
-    if (error !== undefined) response.error = error;
-  
+    if (result !== undefined) {
+        response.result = { value: result };
+    }
+    if (error !== undefined) {
+        response.error = error;
+    }
+
     event.source.postMessage(response, "*");
 });
