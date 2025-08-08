06a67b53cc8b0ab886428fbd9b57f16a31037c8c
KicauID <Kicauid.official@gmail.com>
2025-08-08 13:39:28 +0700

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,27 +1,18 @@
 window.addEventListener("message", async function(event) {
     const { origin, data: { key, params } } = event;
   
-    let result;
-    let error;
+    let result, error;
     try {
-      result = await window.function(...params);
+        result = await window.function(...params);
     } catch (e) {
-      result = undefined;
-      try {
-        error = e.toString();
-      } catch (e) {
-        error = "Exception can't be stringified.";
-      }
+        result = undefined;
+        try { error = e.toString(); }
+        catch { error = "Exception can't be stringified."; }
     }
   
     const response = { key };
-    if (result !== undefined) {
-      // FIXME: Remove `type` once that's in staging
-      response.result = { value: result };
-    }
-    if (error !== undefined) {
-      response.error = error;
-    }
+    if (result !== undefined) response.result = { value: result };
+    if (error !== undefined) response.error = error;
   
     event.source.postMessage(response, "*");
-  });
+});
