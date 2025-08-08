950ff5e7632fd03f45d656744da8fc5b69ae75d9
KicauID <Kicauid.official@gmail.com>
2025-08-08 14:45:07 +0700

    Update function.js

--- function.js
+++ function.js
@@ -13,37 +13,55 @@ window.function = function (htmlParam, fileNameParam) {
     const encodedUserHtml = encodeURIComponent(htmlRaw);
 
     // CSS: lebar tetap 350px, semua margin/padding atas dihapus total
-    const customCSS = `
-      html, body {
-        margin: 0 !important;
-        padding: 0 !important;
-        background: #fff;
-        font-family: Arial, sans-serif;
-      }
-      * {
-        margin-top: 0 !important;
-        padding-top: 0 !important;
-      }
-      .invoice {
-        width: 350px;
-        height: auto;
-        border-bottom: 5px dashed #000;
-        padding: 0;
-        box-sizing: border-box;
-      }
-      #print {
-        width:100%;
+   const customCSS = `
+    body {
+        margin: 0!important;
+    }
+
+    .button {
+        width: 100%;
+        border-radius: 0;
+        font-size: 14px;
+        font-weight: 600;
+        line-height: 1.5rem;
+        color: #ffffff;
         border: none;
-        height:36px;
-        background:#0353A7;
-        color:#fff;
-        font-weight:600;
-        cursor:pointer;
-        position:fixed;
-        top:0;
-        left:0;
-        z-index:9999;
-      }
+        font-family: 'Arial';
+        padding: 0px 12px;
+        height: 32px;
+        text-transform: uppercase;
+        cursor: pointer;
+        box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
+        position: fixed;
+        top: 0;
+        z-index: 1000;
+        background: #0353A7;
+    }
+
+    .button:hover {
+        background: #f5f5f5;
+        color: #000000;
+    }
+
+    .button.printing {
+        background: #ffffff;
+        color: #000000;
+    }
+
+    .button.done {
+        background: #ffffff;
+        color: #000000;
+    }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+        background-color: rgb(0 0 0 / 8%);
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: rgb(0 0 0 / 32%);
+        border-radius: 4px;
+    }
     `;
 
     // HTML akhir
